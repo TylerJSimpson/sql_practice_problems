6acee6bbff912b4d4c5f74d7a46ff70590af954b
Tyler Simpson <94872173+TylerJSimpson@users.noreply.github.com>
2022-03-24 16:41:17 -0400

    Update and rename advanced_mssql_code to advanced_mssql_code.sql

--- advanced_mssql_code.sql
+++ advanced_mssql_code.sql
@@ -1,3 +1,5 @@
+--TO COMPLETE - beginner and intermediate finished
+
 ---------------------------------------------------------
 --SQLserver Northwind Database Advanced Practice Problems
 --25 (32-57) problems in total---------------------------
